1c41a197c9e16807cb80d11e0af1292f89eb1c6a
dychw <25680873+dychw@users.noreply.github.com>
2020-03-21 15:19:14 +0900

    Add views related to login

--- kanban/views.py
+++ kanban/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
-def index(request): 
-    return HttpResponse("仮のトップページ")
+
+def index(request):
+    return render(request, "kanban/index.html")
+
+
+def home(request):
+    return render(request, "kanban/home.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'kanban/signup.html', context)
